83ac38c47d160cecb1ec21d3251373c316e1a36d
Alex J Trevor <atrevor@gmail.com>
2013-07-05 19:39:44 -0400

    Added header file missing from previous commit

---
+++ ros/omnimapper_ros/include/omnimapper_ros/tum_data_error_plugin.h
@@ -0,0 +1,27 @@
+#include <omnimapper/omnimapper_base.h>
+#include <ros/ros.h>
+#include <tf/transform_listener.h>
+#include <tf_conversions/tf_eigen.h>
+#include <visualization_msgs/MarkerArray.h>
+#include <geometry_msgs/Point.h>
+#include <omnimapper_ros/ros_time_utils.h>
+
+namespace omnimapper
+{
+  class TUMDataErrorPlugin : public omnimapper::OutputPlugin
+  {
+    public:
+      TUMDataErrorPlugin (omnimapper::OmniMapperBase* mapper);
+      void update (boost::shared_ptr<gtsam::Values>& vis_values, boost::shared_ptr<gtsam::NonlinearFactorGraph>& vis_graph);
+      
+    protected:
+      ros::NodeHandle nh_;
+      
+      tf::TransformListener tf_listener_;
+
+      ros::Publisher marker_array_pub_;
+
+      OmniMapperBase* mapper_;
+  };
+  
+}
