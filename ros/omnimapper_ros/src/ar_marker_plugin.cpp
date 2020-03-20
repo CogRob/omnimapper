4024d21de5a249233d80495c4ccc333c46875c31
Shengye Wang <i@shengye.wang>
2020-03-19 21:14:48 -0700

    Done. That's all.

--- ros/omnimapper_ros/src/ar_marker_plugin.cpp
+++ ros/omnimapper_ros/src/ar_marker_plugin.cpp
@@ -11,16 +11,16 @@ namespace omnimapper
   {
     marker_sub_ = nh_.subscribe ("/ar_pose_marker", 1, &ARMarkerPlugin::markerCallback, this);
   }
-  
-  void 
+
+  void
   ARMarkerPlugin::markerCallback (const ar_track_alvar_msgs::AlvarMarkers& msg)
   {
     ROS_INFO ("Got some markers: %d\n", msg.markers.size ());
-    
+
     //omnimapper::Time msg_time = omnimapper::rostime2ptime (msg.header.stamp);
     // HACK since at_track_alvar does not fill in header stamp
     omnimapper::Time msg_time = omnimapper::rostime2ptime (ros::Time::now ());
-    
+
     gtsam::Symbol pose_symbol;
     mapper_->getPoseSymbolAtTime (msg_time, pose_symbol);
 
@@ -41,14 +41,14 @@ namespace omnimapper
         mapper_->addNewValue (marker_symbol, relative_pose);
         known_markers_.insert (msg.markers[i].id);
       }
-      
+
       gtsam::SharedDiagonal measurement_noise;
-      measurement_noise = gtsam::noiseModel::Diagonal::Sigmas (gtsam::Vector_ (6, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1));
+      measurement_noise = gtsam::noiseModel::Diagonal::Sigmas (gtsam::repeat(6, 0.1));
 
       omnimapper::OmniMapperBase::NonlinearFactorPtr marker_factor (new gtsam::BetweenFactor<gtsam::Pose3> (pose_symbol, marker_symbol, relative_pose, measurement_noise));
       mapper_->addFactor (marker_factor);
     }
-    
+
   }
-  
+
 }
