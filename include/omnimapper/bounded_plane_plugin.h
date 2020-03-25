83e5321a22bf9a4106280a66dd0242acc1a85085
Shengye Wang <i@shengye.wang>
2020-03-24 21:46:15 -0700

    Run clang format.

--- include/omnimapper/bounded_plane_plugin.h
+++ include/omnimapper/bounded_plane_plugin.h
@@ -1,54 +1,74 @@
-#include <pcl/point_cloud.h>
-#include <pcl/point_types.h>
-#include <pcl/segmentation/planar_region.h>
-#include <omnimapper/omnimapper_base.h>
 #include <omnimapper/BoundedPlane3.h>
 #include <omnimapper/BoundedPlaneFactor.h>
 #include <omnimapper/get_transform_functor.h>
+#include <omnimapper/omnimapper_base.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/segmentation/planar_region.h>
 
-namespace omnimapper
-{
-  template <typename PointT>
-  class BoundedPlanePlugin
-  {
-    typedef typename pcl::PointCloud<PointT> Cloud;
-    typedef typename Cloud::Ptr CloudPtr;
-    typedef typename Cloud::ConstPtr CloudConstPtr;    
+namespace omnimapper {
+template <typename PointT>
+class BoundedPlanePlugin {
+  typedef typename pcl::PointCloud<PointT> Cloud;
+  typedef typename Cloud::Ptr CloudPtr;
+  typedef typename Cloud::ConstPtr CloudConstPtr;
 
-    public:
-      BoundedPlanePlugin (omnimapper::OmniMapperBase* mapper);
-      
-      /** \brief regionsToMeasurements converts a set of planar regions as extracted by PCL's organized segmentation tools into a set of Planar landmark measurements suitable for use with the OmniMapper. */
-      void regionsToMeasurements (std::vector<pcl::PlanarRegion<PointT>, Eigen::aligned_allocator<pcl::PlanarRegion<PointT> > >& regions, omnimapper::Time t, std::vector<omnimapper::BoundedPlane3<PointT> >& plane_measurements);
+ public:
+  BoundedPlanePlugin(omnimapper::OmniMapperBase* mapper);
 
-      void removeDuplicatePoints (pcl::PointCloud<PointT>& boundary_cloud);
+  /** \brief regionsToMeasurements converts a set of planar regions as extracted
+   * by PCL's organized segmentation tools into a set of Planar landmark
+   * measurements suitable for use with the OmniMapper. */
+  void regionsToMeasurements(
+      std::vector<pcl::PlanarRegion<PointT>,
+                  Eigen::aligned_allocator<pcl::PlanarRegion<PointT> > >&
+          regions,
+      omnimapper::Time t,
+      std::vector<omnimapper::BoundedPlane3<PointT> >& plane_measurements);
 
-      bool polygonsOverlap (CloudPtr boundary1, CloudPtr boundary2);
+  void removeDuplicatePoints(pcl::PointCloud<PointT>& boundary_cloud);
 
-      bool polygonsOverlapBoost (Eigen::Vector4d& coeffs1, CloudPtr boundary1, Eigen::Vector4d& coeffs2, CloudPtr boundary2);
+  bool polygonsOverlap(CloudPtr boundary1, CloudPtr boundary2);
 
-      /** \brief planarRegionCallback receives segmented data from the segmentation. */
-      void planarRegionCallback (std::vector<pcl::PlanarRegion<PointT>, Eigen::aligned_allocator<pcl::PlanarRegion<PointT> > > regions, omnimapper::Time t);
+  bool polygonsOverlapBoost(Eigen::Vector4d& coeffs1, CloudPtr boundary1,
+                            Eigen::Vector4d& coeffs2, CloudPtr boundary2);
 
-      /** \brief setAngularThreshold sets the angular threshold to be used for data association. */
-      void setAngularThreshold (double angular_threshold) { angular_threshold_ = angular_threshold; }
-     
-      /** \brief setRangeThreshold sets the range threshold to be used for data association. */
-      void setRangeThreshold (double range_threshold) { range_threshold_ = range_threshold; }
+  /** \brief planarRegionCallback receives segmented data from the segmentation.
+   */
+  void planarRegionCallback(
+      std::vector<pcl::PlanarRegion<PointT>,
+                  Eigen::aligned_allocator<pcl::PlanarRegion<PointT> > >
+          regions,
+      omnimapper::Time t);
 
-      void setAngularNoise (double angular_noise) { angular_noise_ = angular_noise; }
+  /** \brief setAngularThreshold sets the angular threshold to be used for data
+   * association. */
+  void setAngularThreshold(double angular_threshold) {
+    angular_threshold_ = angular_threshold;
+  }
 
-      void setRangeNoise (double range_noise) { range_noise_ = range_noise; }
+  /** \brief setRangeThreshold sets the range threshold to be used for data
+   * association. */
+  void setRangeThreshold(double range_threshold) {
+    range_threshold_ = range_threshold;
+  }
 
-      void setSensorToBaseFunctor (omnimapper::GetTransformFunctorPtr get_transform) { get_sensor_to_base_ = get_transform; }
+  void setAngularNoise(double angular_noise) { angular_noise_ = angular_noise; }
 
-    protected:
-      OmniMapperBase* mapper_;
-      GetTransformFunctorPtr get_sensor_to_base_;
-      int max_plane_id_;
-      double angular_threshold_;
-      double range_threshold_;
-      double angular_noise_;
-      double range_noise_;
-  };
-}
+  void setRangeNoise(double range_noise) { range_noise_ = range_noise; }
+
+  void setSensorToBaseFunctor(
+      omnimapper::GetTransformFunctorPtr get_transform) {
+    get_sensor_to_base_ = get_transform;
+  }
+
+ protected:
+  OmniMapperBase* mapper_;
+  GetTransformFunctorPtr get_sensor_to_base_;
+  int max_plane_id_;
+  double angular_threshold_;
+  double range_threshold_;
+  double angular_noise_;
+  double range_noise_;
+};
+}  // namespace omnimapper
--- ros/omnimapper_ros/include/omnimapper_ros/csm_visualizer.h
+++ ros/omnimapper_ros/include/omnimapper_ros/csm_visualizer.h
@@ -1,50 +1,53 @@
 #pragma once
 
+#include <geometry_msgs/Point.h>
 #include <omnimapper/omnimapper_base.h>
+#include <omnimapper_ros/VisualizeFullCloud.h>
 #include <omnimapper_ros/canonical_scan_matcher_plugin.h>
 #include <ros/ros.h>
 #include <tf_conversions/tf_eigen.h>
 #include <visualization_msgs/MarkerArray.h>
-#include <geometry_msgs/Point.h>
-#include <omnimapper_ros/VisualizeFullCloud.h>
 
-namespace omnimapper
-{
-  template <typename LScanT>
-  class CSMVisualizerRViz : public omnimapper::OutputPlugin
-  {
-    public:
-      CSMVisualizerRViz (omnimapper::OmniMapperBase* mapper);
-      void update (boost::shared_ptr<gtsam::Values>& vis_values, boost::shared_ptr<gtsam::NonlinearFactorGraph>& vis_graph);
-      void setCSMPlugin (boost::shared_ptr<omnimapper::CanonicalScanMatcherPlugin<LScanT> >& csm_plugin) { csm_plugin_ = csm_plugin; }
-      bool drawCSMMap (omnimapper_ros::VisualizeFullCloud::Request &req, omnimapper_ros::VisualizeFullCloud::Response &res);
-      
-    protected:
-      ros::NodeHandle nh_;
-      
-      OmniMapperBase* mapper_;
-      
-      ros::Publisher pose_array_pub_;
+namespace omnimapper {
+template <typename LScanT>
+class CSMVisualizerRViz : public omnimapper::OutputPlugin {
+ public:
+  CSMVisualizerRViz(omnimapper::OmniMapperBase* mapper);
+  void update(boost::shared_ptr<gtsam::Values>& vis_values,
+              boost::shared_ptr<gtsam::NonlinearFactorGraph>& vis_graph);
+  void setCSMPlugin(
+      boost::shared_ptr<omnimapper::CanonicalScanMatcherPlugin<LScanT> >&
+          csm_plugin) {
+    csm_plugin_ = csm_plugin;
+  }
+  bool drawCSMMap(omnimapper_ros::VisualizeFullCloud::Request& req,
+                  omnimapper_ros::VisualizeFullCloud::Response& res);
 
-      ros::Publisher marker_array_pub_;
-      
-      ros::Publisher map_cloud_pub_;
+ protected:
+  ros::NodeHandle nh_;
 
-      ros::ServiceServer draw_csm_map_srv_;
-      
-      boost::shared_ptr<omnimapper::CanonicalScanMatcherPlugin<LScanT> > csm_plugin_;
+  OmniMapperBase* mapper_;
 
-      boost::shared_ptr<gtsam::Values> vis_values_;
+  ros::Publisher pose_array_pub_;
 
-      boost::shared_ptr<gtsam::NonlinearFactorGraph> vis_graph_;
+  ros::Publisher marker_array_pub_;
 
-      boost::mutex vis_mutex_;
+  ros::Publisher map_cloud_pub_;
 
-      bool draw_graph_;
+  ros::ServiceServer draw_csm_map_srv_;
 
-      bool draw_map_;
+  boost::shared_ptr<omnimapper::CanonicalScanMatcherPlugin<LScanT> >
+      csm_plugin_;
 
-      
-  };
-  
-}
+  boost::shared_ptr<gtsam::Values> vis_values_;
+
+  boost::shared_ptr<gtsam::NonlinearFactorGraph> vis_graph_;
+
+  boost::mutex vis_mutex_;
+
+  bool draw_graph_;
+
+  bool draw_map_;
+};
+
+}  // namespace omnimapper
